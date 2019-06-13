9641d13e703f39e143549c8f1c93a633ccf6133f
330 <Achilles0915@yandex.com>
2019-06-13 01:15:58 -0700

    clear

--- HTML5RecorderASPWEBFORM/HTML5RecorderApp/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace HTML5RecorderApp.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- HTML5RecorderASPWEBFORM/HTML5RecorderApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HTML5RecorderApp.Global" Language="C#" %>
